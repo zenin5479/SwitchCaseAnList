fbf1d5309cae31a33a1356840b48f0add277c7b0
zenin5479 <zenin5479@gmail.com>
2023-10-26 23:00:12 +0300

    Update 0.1 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -4,6 +4,34 @@
     {
         static void Main(string[] args)
         {
+            var myList = new List<string> { "Apple", "Pear" };
+            switch (myList)
+            {
+                case var _ when myList.Contains("Apple"):
+                    // do apple stuff
+                    break;
+                case var _ when myList.Contains("Pear"):
+                    // do pear stuff
+                    break;
+                default:
+                    throw new ArgumentException("Some error message", nameof(myList));
+            }
+
+
+            var numbers = new int[] { 10, 20, 30 };
+            Console.WriteLine(GetSourceLabel(numbers));  // output: 1
+
+            var letters = new List<char> { 'a', 'b', 'c', 'd' };
+            Console.WriteLine(GetSourceLabel(letters));  // output: 2
+
+            static int GetSourceLabel<T>(IEnumerable<T> source) => source switch
+            {
+                Array array => 1,
+                ICollection<T> collection => 2,
+                _ => 3,
+            };
+
+
             
         }
     }
